d087f612ba7ef5c3a12659cf9fd70cbccfcef0f6
Miguel N <miguelnr89[arroba]gmail[punto]com>
2012-12-31 00:18:03 +0100

    minimal changes

--- program_help.py
+++ program_help.py
@@ -16,54 +16,63 @@ __copyright__ = "Copyright 2012, PyASB project"
 __credits__ = ["Miguel Nievas"]
 __license__ = "GNU GPL v3"
 __shortname__ = "PyASB"
-__longname__ = "Python All-Sky Brightness measuring pipeline"
+__longname__ = "Python All-Sky Brightness pipeline"
 __version__ = "1.99.0"
 __maintainer__ = "Miguel Nievas"
 __email__ = "miguelnr89[at]gmail[dot]com"
 __status__ = "Prototype" # "Prototype", "Development", or "Production"
 
 class PlatformHelp():
-	def __init__():
+	def __init__(self):
 		self.make_title()
 		self.make_welcome()
 		self.make_requisites()
 		self.make_options()
 	
 	def make_title(self):
-		nameversion = 5*'#'+__shortname__+' v'+__version__+5*'#'
+		nameversion = 10*'#'+3*' '+__shortname__+' v'+__version__+3*' '+10*'#'
 		self.separator = len(nameversion)*'#'
-		self.title = +'\n'+nameversion+'\n'+self.separator+'\n'
+		self.title = nameversion
 	
 	def make_welcome(self):
-		self.welcome = 'Welcome to '+__shortname__+' ('+longname+')\n'
+		self.welcome = 'Welcome to '+__shortname__+' ('+__longname__+')\n'
 	
 	def make_requisites(self):
-		self.requisites = 'Requisites: Python 2.7; Scipy; Numpy; Matplotlib; Pyfits; Pyephem\n'
+		self.requisites = \
+			'Requisites: Python 2.7; Scipy; Numpy;\n'+\
+			'  Matplotlib; Pyfits; Pyephem\n\n'
 	
 	def make_options(self):
 		self.options = \
-			'
-			-h: print this help message\n\n
-			-i input_allsky_image: All Sky image you want to be analyzed\n\n
-			-f [year,month,day]: Date to be analyzed (AstMon-UCM only)\n
-			  month and day are optional\n\n
-			-om output_map_image: Output star map image, full or relative path\n
-			  if no output file, show the map on screen\n\n
-			-ot output_photometric_table: Output photometric table full or relative path\n
-			  if no output file, show the table on screen\n\n
-			-or output_results_summary: Summary of analysis, fit parameters and zenith SB\n
-			  full or relative path. If no output file, show the table on screen\n\n
-			-ob output_bouguerfit_graph: Output bouguer-fit graph, full or relative path\n
-			  if no output file, show the graph on screen\n\n
-			-os output_skybrightness_graph: Output Sky Brightness graph, full or relative path\n
-			  if no output file, show the graph on screen\n\n
-			-ost output_skybrightness_table: Output Sky Brightness table, full or relative path\n
-			  if no output file, show the graph on screen\n\n
-			'
+			'-h: print this help message\n\n'+\
+			'-i input_allsky_image: \n'+\
+			'  All Sky image you want to be analyzed\n\n'+\
+			#'-f [year,month,day]:\n'+\
+			#'  Date to be analyzed (AstMon-UCM only)\n'+\
+			#'  month and day are optional\n\n'+\
+			'-om output_map_image:\n'+\
+			'  Output star map image, full or relative path\n'+\
+			'  if no output file, show the map on screen\n\n'+\
+			'-ot output_photometric_table:\n'+\
+			'  Output photometric table full or relative path\n'+\
+			'  if no output file, show the table on screen\n\n'+\
+			'-or output_results_summary:\n'+\
+			'  Summary of analysis, fit parameters and zenith SB\n'+\
+			'  full or relative path. If no output file, \n'+\
+			'  show the table on screen\n\n'+\
+			'-ob output_bouguerfit_graph:\n'+\
+			'  Output bouguer-fit graph, full or relative path.\n'+\
+			'  If no output file, show the graph on screen\n\n'+\
+			'-os output_skybrightness_graph:\n'+\
+			'  Output Sky Brightness graph, full or relative path\n'+\
+			'  if no output file, show the graph on screen\n\n'+\
+			'-ost output_skybrightness_table:\n'+\
+			'  Output Sky Brightness table, full or relative path\n'+\
+			'  if no output file, show the graph on screen\n\n'
 	
-	def show_help():
+	def show_help(self):
 		print(\
-			self.separator+'\n'+self.title+self.separator+'\n'+self.welcome+\
+			self.separator+'\n'+self.title+'\n'+self.separator+'\n'+self.welcome+\
 			self.requisites+self.options+self.separator)
 			
 	def incorrect_parameter(self,parameter):
--- pyasb_launcher.py
+++ pyasb_launcher.py
@@ -16,18 +16,36 @@ __copyright__ = "Copyright 2012, PyASB project"
 __credits__ = ["Miguel Nievas"]
 __license__ = "GNU GPL v3"
 __shortname__ = "PyASB"
-__longname__ = "Python All-Sky Brightness measuring pipeline"
+__longname__ = "Python All-Sky Brightness pipeline"
 __version__ = "1.99.0"
 __maintainer__ = "Miguel Nievas"
 __email__ = "miguelnr89[at]gmail[dot]com"
 __status__ = "Prototype" # "Prototype", "Development", or "Production"
 
 
+try:
+	from read_config import *
+	from program_help import *
+	from astrometry import *
+	from star_calibration import *
+	from load_fitsimage import *
+	from bouguer_fit import *
+	from sky_brightness import *
+	from skymap_plot import *
+except:
+	print 'One or more modules missing: please check'
+	raise SystemExit
+
+
 config_filename = 'pyasb_config.txt'
 
-
 class InstrumentCalibration():
-	def __init__():
+	def __init__(Image):
+		#ConfigOptions = ConfigOptions(config_name)
+		FitsImage = FitsImage(InputFile)
+		ImageInfo = ImageInfo(fits_Header,config_filename)
+		PlatformHelp = PlatformHelp()
+		
 		
 		
 		
